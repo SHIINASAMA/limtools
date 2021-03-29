205914efd8f934eb005121235b9e82a8859b198a
kaoru <shiina_kaoru@outlook.com>
2021-03-30 00:36:41 +0800

    🧪test: 线程池测试
    完成FixedThreadPool在linux下的测试

--- test/test.cpp
+++ test/test.cpp
@@ -10,23 +10,31 @@ public:
     virtual void Run() override
     {
         //Console::Write("任务被执行\n");
+#ifdef _WIN32
         _sleep(1000);
+#elif __linux__
+        sleep(1);
+#endif
         done = true;
     }
 };
 
 int main()
 {
-    FixedThreadPool::CreateFixedThreadPool(1000, false);
+    FixedThreadPool::CreateFixedThreadPool(100, false);
     auto pool = FixedThreadPool::GetFixedThreadPool();
     pool->Run();
-    MyTask *tasks = new MyTask[3000];
-    for (int i = 0; i < 3000; i++)
+    MyTask *tasks = new MyTask[1000];
+    for (int i = 0; i < 1000; i++)
     {
         pool->Push(&tasks[i]);
     }
 
+#ifdef _WIN32
     _sleep(5000);
+#elif __linux__
+    sleep(5);
+#endif
     pool->Exit();
     Console::Write("线程池已关闭\n");
     return 0;
