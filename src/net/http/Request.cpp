f6ac5ba5e3c3c90568180cc23acbe9156b3c3627
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2021-04-07 23:07:15 +0800

    ✨feat: 重构和修复
    重构Request::Post方法
    修复Request一次读取报文过长导致数据丢失的问题

--- src/net/http/Request.cpp
+++ src/net/http/Request.cpp
@@ -25,6 +25,10 @@ Request::Request(const char *url, const char *ipaddr, unsigned short port)
 
 Request::~Request()
 {
+    if (this->response != nullptr)
+    {
+        delete this->response;
+    }
 }
 
 Response *Request::Get()
@@ -53,12 +57,12 @@ Response *Request::Get()
     int recv_lenght = socket->Read(recv_buf, 4096);
 
     //报文头
-    int status_line_lenght = 0;
+    int status_line_length = 0;
     for (int i = 0; i < recv_lenght; i++)
     {
         if (recv_buf[i] == '\r' && recv_buf[i + 1] == '\n')
         {
-            status_line_lenght = i + 1;
+            status_line_length = i + 1;
             break;
         }
     }
@@ -68,9 +72,9 @@ Response *Request::Get()
                                  (recv_buf[10] - 48) * 10 +
                                  (recv_buf[11] - 48);
 
-    int header_length = 0;
     //报文参数
-    int prepos = status_line_lenght + 1;
+    int header_length = 0;
+    int prepos = status_line_length + 1;
     int pos1 = prepos;
     int pos2 = prepos;
     for (int i = prepos; i < recv_lenght; i++)
@@ -102,17 +106,41 @@ Response *Request::Get()
 
             if (recv_buf[i] == '\r' && recv_buf[i + 1] == '\n')
             {
+                header_length = i + 2;
                 break;
             }
         }
     }
 
-    if (this->response->Args.count((char*)"Content-Length") == 1)
+    //读取不完整，做失败处理
+    if (header_length == 0)
+    {
+        return nullptr;
+    }
+
+    if (this->response->Args.count((char *)"Content-Length") == 1)
     {
         this->response->ContentLength = atol(this->response->Args[(char *)"Content-Length"]);
-
         this->response->Content = new char[this->response->ContentLength + 1];
-        socket->Read(this->response->Content, this->response->ContentLength);
+
+        //报头完整且包含正文
+        if (recv_lenght - header_length >= 0)
+        {
+            memcpy(this->response->Content, &recv_buf[header_length], recv_lenght - header_length);
+        }
+
+        //循环读取避免超过最大长度
+        int surplus_length = this->response->ContentLength - (recv_lenght - header_length);
+        while (surplus_length)
+        {
+            int l = socket->Read(&this->response->Content[this->response->ContentLength - surplus_length], 4096);
+            if (!l)
+            {
+                return nullptr;
+            }
+
+            surplus_length -= l;
+        }
         this->response->Content[this->response->ContentLength] = '\0';
     }
 
@@ -123,6 +151,126 @@ Response *Request::Get()
 
 Response *Request::Post(const char *content, int size)
 {
+    //请求行以及参数
+    char request_line[1024]{0};
+    sprintf(request_line, "POST %s HTTP/1.1\r\n", this->url);
+    this->Args.insert(0, request_line);
+    this->Args.append("\r\n");
+
+    //连接服务器
+    auto socket = new Socket(SocketMode::Client, this->ipaddr, this->port);
+    if (socket->Connect())
+    {
+        return nullptr;
+    }
+
+    //发送请求头
+    if (!socket->Write(this->Args.c_str(), this->Args.length()))
+    {
+        return nullptr;
+    }
+
+    //POST报文
+    if (!socket->Write(content, size))
+    {
+        return nullptr;
+    }
+
+    this->response = new Response();
+    char recv_buf[4096]{0};
+    int recv_lenght = socket->Read(recv_buf, 4096);
+
+    //报文头
+    int status_line_length = 0;
+    for (int i = 0; i < recv_lenght; i++)
+    {
+        if (recv_buf[i] == '\r' && recv_buf[i + 1] == '\n')
+        {
+            status_line_length = i + 1;
+            break;
+        }
+    }
+
+    //状态码
+    this->response->StatusCode = (recv_buf[9] - 48) * 100 +
+                                 (recv_buf[10] - 48) * 10 +
+                                 (recv_buf[11] - 48);
+
+    //报文参数
+    int header_length = 0;
+    int prepos = status_line_length + 1;
+    int pos1 = prepos;
+    int pos2 = prepos;
+    for (int i = prepos; i < recv_lenght; i++)
+    {
+        //键值分割点
+        if (recv_buf[i] == ':' && recv_buf[i + 1] == ' ')
+        {
+            pos1 = i - 1;
+            i += 2;
+        }
+        //行末
+        else if (recv_buf[i] == '\r' && recv_buf[i + 1] == '\n')
+        {
+            prepos = pos2;
+            pos2 = i + 2;
+            i += 2;
+
+            int key_lenght = pos1 - prepos + 1;
+            char *key = new char[key_lenght + 1];
+            memcpy(key, &recv_buf[prepos], key_lenght);
+            key[key_lenght] = '\0';
+
+            int value_lenght = pos2 - pos1 - 5;
+            char *value = new char[value_lenght + 1];
+            memcpy(value, &recv_buf[pos1 + 3], value_lenght);
+            value[value_lenght] = '\0';
+
+            this->response->Args.insert(std::make_pair(key, value));
+
+            if (recv_buf[i] == '\r' && recv_buf[i + 1] == '\n')
+            {
+                header_length = i + 2;
+                break;
+            }
+        }
+    }
+
+    //读取不完整，做失败处理
+    if (header_length == 0)
+    {
+        return nullptr;
+    }
+
+    if (this->response->Args.count((char *)"Content-Length") == 1)
+    {
+        this->response->ContentLength = atol(this->response->Args[(char *)"Content-Length"]);
+        this->response->Content = new char[this->response->ContentLength + 1];
+
+        //报头完整且包含正文
+        if (recv_lenght - header_length >= 0)
+        {
+            memcpy(this->response->Content, &recv_buf[header_length], recv_lenght - header_length);
+        }
+
+        //循环读取避免超过最大长度
+        int surplus_length = this->response->ContentLength - (recv_lenght - header_length);
+        while (surplus_length)
+        {
+            int l = socket->Read(&this->response->Content[this->response->ContentLength - surplus_length], 4096);
+            if (!l)
+            {
+                return nullptr;
+            }
+
+            surplus_length -= l;
+        }
+        this->response->Content[this->response->ContentLength] = '\0';
+    }
+
+    socket->Close();
+    delete socket;
+    return this->response;
 }
 
 void Request::SetArg(const char *key, const char *value)
--- test/test.cpp
+++ test/test.cpp
@@ -3,29 +3,37 @@
 
 int main()
 {
-    Request *get = new Request("/", "220.181.38.148", 80);
+    char *temp[1];
+    if (Socket::GetHostByName(temp, 1, "www.baidu.com") <= 0)
+    {
+        Console::Print("域名解析失败\n");
+        return -1;
+    }
+
+    const char *ipaddr = temp[0];
+
+    Request *get = new Request("/", ipaddr, 80);
     get->SetArg("Host", "www.baidu.com");
     auto res1 = get->Get();
     for (auto itor = res1->Args.begin(); itor != res1->Args.end(); itor++)
     {
         Console::Write("%s: %s\n", itor->first, itor->second);
     }
-    Console::Print(res1->Content);
+    Console::Write("%d == %d\n", res1->ContentLength, strlen(res1->Content));
     delete get;
 
-    // Console::NewLine();
+    Console::NewLine();
 
-    // Request *post = new Request("/", "112.80.248.75\0", 80);
-    // post->SetArg("Host", "www.baidu.com");
-    // post->SetArg("Content-Lenght", "5");
-    // auto res2 = post->Post("Hello", 5);
-    // for (auto itor = res2->recvMap.begin(); itor != res2->recvMap.end(); itor++)
-    // {
-    //     Console::Write("%s: %s\n", itor->first, itor->second);
-    // }
-    // Console::Print(res2->content);
-    // int len = strlen(res2->content);
-    // delete post;
+    Request *post = new Request("/", ipaddr, 80);
+    post->SetArg("Host", "www.baidu.com");
+    post->SetArg("Content-Lenght", "5");
+    auto res2 = post->Post("Hello", 5);
+    for (auto itor = res2->Args.begin(); itor != res2->Args.end(); itor++)
+    {
+        Console::Write("%s: %s\n", itor->first, itor->second);
+    }
+    Console::Write("%d == %d\n", res2->ContentLength, strlen(res2->Content));
+    delete post;
 
     return 0;
 }
